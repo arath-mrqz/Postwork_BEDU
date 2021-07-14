448e4d6d9330154d575a1ec8ad650ff07b1d814b
Juan Rivera Vargas <71957386+JuanR-V@users.noreply.github.com>
2021-07-14 17:29:41 -0500

    Update Postwork6-8.R

--- Postwork6-8/Postwork6-8.R
+++ Postwork6-8/Postwork6-8.R
@@ -10,15 +10,13 @@ library(dplyr)
         data$sumagoles = data$home.score + data$away.score
 
 #Punto 2 Obtén el promedio por mes de la suma de goles.
-        #Agrupamos por mes y año
+        
         data <- mutate(data, Ym = format(date, "%Y-%m"))
+        #Movemos los datos del mes de Junio al mes de mayo
+        data[data$Ym=="2013-06",7]<-"2013-05"
+        #Agrupamos por mes y año
         data.promedios <- data %>% group_by(Ym) %>% summarise(goles = mean(sumagoles))
         
-        #Eliminamos el unico promedio de junio que tenemos para poder usar una frecuencia de 10
-        data.promedios<-data.promedios[data.promedios$Ym!="2013-06",]
-        #Tambien eliminamos los promedios de los meses del 2020
-        data.promedios<-data.promedios[-c(96,97, 98, 99,100,101), ]
-
 # Punto 3 Crea la serie de tiempo del promedio por mes de la suma de goles hasta diciembre de 2019.
         #Utilizamos una frecuencia de 10 debido a que en Junio y Julio no hay partidos
         promedio.ts <- ts(data.promedios$goles, start = c(2010, 8), end = c(2019, 9), # Hasta diciembre de 2019
@@ -26,23 +24,20 @@ library(dplyr)
 
 # Punto 4 Grafica la serie de tiempo.
         plot(promedio.ts, col = 9, ylim = c(1.7, 3.5),  xlab = "Fecha",ylab = "Goles promedio",
-                main = "Serie de tiempo de promedio de goles por mes",
+                main = "Serie de tiempo del promedio de goles por mes",
                 sub = "Agosto de 2010 a Diciembre de 2019",
                 col.main= "brown",
                 col.sub="brown",
                 fg = "orange"
                      )
-        #linea en la media
                 abline(h = mean(promedio.ts), lwd = 2, col = 2, lty = 2)
-        #linea para la apuesta mas segura
-                abline(h = 2, lwd = 2, col = 3, lty = 2)
+
                 legend("bottomleft", legend = paste("Media= ",round(mean(promedio.ts),digits=2)),
                        lty = 2,
                        bty = "n", # Elimina la línea de la caja,
                        lwd = 2, col = c("red"),
                        cex = .8)
-
-
+# Notas para los datos de soccer: https://www.football-data.co.uk/notes.txt
 
 
 ## POSTWORK 7 ##
